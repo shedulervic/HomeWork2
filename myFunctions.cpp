ac292aba16dd428dee9fa62f43ff9655b07e0674
shedulervic <sheduler.vic@gmail.com>
2021-05-27 18:02:58 +0300

    All homework 5!

--- myFunctions.cpp
+++ myFunctions.cpp
@@ -1,5 +1,9 @@
 #include "myFunctions.h"
 #include <iostream>
+#include <cstdarg>
+
+//** Написать все функции в отдельных файлах в одном пространстве имён, вызвать их на исполнение в основном файле программы используя указатели на функции.
+
 
 void myFunc::ReverseMassiveElement(int a[], int num)
 {
@@ -10,8 +14,6 @@ void myFunc::ReverseMassiveElement(int a[], int num)
         std::cout << "new: " << a[i] << std::endl;
     }
 }
-
-
 
 void myFunc::InitMassiveElements(int* a, int SZ)
 {
@@ -47,4 +49,79 @@ void myFunc::CheckMassiveSum(int* a, int SZ3)
     std::cout << (answ ? "true" : "false") << std::endl;
    
     
+}
+
+void myFunc::CicleShift(int* a, int ln)
+{
+    const int SZ = 5;
+    int b[SZ]{};
+    if (ln < 0)
+    {
+        ln = -ln;
+        for (int i = 0; i < SZ; i++)
+        {
+            b[i] = a[i];
+        }
+        for (int i = 0; i < SZ; i++)
+        {
+            if (i < ln)
+            {
+                int t = i + ln +1;
+                a[t] = b[i];
+            }
+            else
+            {
+                int t = i - ln;
+                a[t] = b[i];
+            }
+        }
+        for (int i = 0; i < SZ; i++)
+        {
+            std::cout << a[i] << " ";
+        }
+
+    }
+    else
+    {
+        for (int i = 0; i < SZ; i++)
+        {
+            b[i] = a[i];
+        }
+        for (int i = 0; i < SZ; i++)
+        {
+            if (ln < SZ - i)
+            {
+                int t = i + ln;
+                a[t] = b[i];
+            }
+            else
+            {
+                int t = (i + ln) % SZ;
+                a[t] = b[i];
+            }
+        }
+        for (int i = 0; i < SZ; i++)
+        {
+            std::cout << a[i] << " ";
+        }
+    }
+    
+    std::cout << std::endl;
+}
+
+void myFunc::ReverseMassiveElementVar(int a[], int SZ5, ...)
+{
+    float result = 0;
+    va_list lst;
+    va_start(lst, SZ5);
+    for (int i = 0; i < SZ5; i++)
+    {
+        result += va_arg(lst, int);
+    }
+    for (int i = 0; i < SZ5; i++)
+    {
+        std::cout << "old: " << a[i] << " ";
+        a[i] = 1 - a[i];
+        std::cout << "new: " << a[i] << std::endl;
+    }
 }
--- myFunctions.h
+++ myFunctions.h
@@ -5,4 +5,6 @@ namespace myFunc
 	void ReverseMassiveElement(int a[], int num);
 	void InitMassiveElements(int* a, int SZ);
 	void CheckMassiveSum(int* a, int SZ3);
+	void CicleShift(int* a, int ln);
+	void ReverseMassiveElementVar(int SZ5a[], int SZ5, ...);
 }
