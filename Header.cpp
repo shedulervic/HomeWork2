4fa7994b9400630b368a4cb732a5131c3196db79
shedulervic <sheduler.vic@gmail.com>
2021-06-03 14:30:26 +0300

    secondTask

--- Header.cpp
+++ Header.cpp
@@ -17,7 +17,34 @@ void fileFunctions::WriteFile(std::string FileName)
 	std::ofstream out(FileName, std::ios::app);
 	if (in.is_open())
 	{
-		out << in.rdbuf(); //про rbuf нарыл в инете но пока не разобрался что он на самом деле такое
+		out << in.rdbuf(); //про rbuf нарыл в инете docs.microsoft.com/ru-ru/previous-versions/k7hz8258(v=vs.140)
 	}
 	in.close();
+}
+
+void fileFunctions::GluingFile(std::string FileName1, std::string FileName2)
+{
+	namespace fs = std::filesystem;
+	 // получим размер файлов
+	std::ifstream one(FileName1);
+	int sizeone = fs::file_size(fs::current_path() / FileName1);
+	std::ifstream two(FileName2);
+	int sizetwo = fs::file_size(fs::current_path() / FileName2);
+	// выделяем память
+	char *textOne = (char*) calloc(sizeone, sizeof(int));
+	char* textTwo = (char*)calloc(sizetwo, sizeof(int));
+	std::ofstream textGluing;
+    // буфферизация
+	while (!one.eof())
+	{
+		one.getline(textOne, sizeone);
+	}
+	while (!two.eof())
+	{
+		two.getline(textTwo, sizetwo);
+	}
+	//не понял почему при использовании eof попадает только первое предложение
+	//записываем в файл
+	std::ofstream gluingFile("Gluing.txt", std::ios::app);
+	gluingFile << textOne << textTwo;
 }
--- Header.h
+++ Header.h
@@ -5,4 +5,5 @@ namespace fileFunctions
 {
 	void CreateFile(std::string FileName);
 	void WriteFile(std::string FileName);
+	void GluingFile(std::string FileName1, std::string FileName2);
 }
